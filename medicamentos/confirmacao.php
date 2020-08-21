b578fd4e9e0873b6f224adaa13894ad2d623a4e6
Juliana Câmara <julianabc@id.uff.br>
2020-08-21 20:11:26 -0300

    Create confirmacao.php

---
+++ medicamentos/confirmacao.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Confirmação</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	
+	<p>Dados Cadastrados! Caso queira continuar o seu cadastramento, <a href="questao2.php">clique aqui.</a></p>
+	
+</body>
+</html>
